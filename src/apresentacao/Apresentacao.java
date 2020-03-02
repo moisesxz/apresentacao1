8f163ecdd219996b162413e2ba61ade955232065
Moises Santos <Moises Santos@DESKTOP-OCUQ1EB>
2020-03-02 08:37:57 -0300

    fiz uma pequena alteração

--- src/apresentacao/Apresentacao.java
+++ src/apresentacao/Apresentacao.java
@@ -15,6 +15,7 @@ public class Apresentacao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("bom dia amigo");
         // TODO code application logic here
     }
     
